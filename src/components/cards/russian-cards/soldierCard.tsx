d8be6636f734a3ad8805ddce225c7bc088b112de
DimaMaslo88 <dmitrij-maslo@mail.ru>
2025-04-01 22:15:06 +0300

    'feat/warrior card'

--- src/components/cards/russian-cards/soldierCard.tsx
+++ src/components/cards/russian-cards/soldierCard.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import s from 'styles/solderCard.module.scss';
-import image from 'images/russian-images/partizan.jpg';
+import image from 'images/russian-images/partizan.webp';
+import damageIcon from 'images/russian-images/pngegg (1).png';
 
 export const SoldierCard = () => (
   <div className={s.card}>
     <h2 className={s.title}>Ополченец</h2>
-    <img src={image} />
+    <img src={image} alt="warrior" />
     <p>
-      урон
+      <img src={damageIcon} alt="damage" className={s.icons} title="Урон" />
     </p>
   </div>
 );
