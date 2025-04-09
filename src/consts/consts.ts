2042bf89de8988a091a1854804d8053d97f3c0b0
DimaMaslo88 <dmitrij-maslo@mail.ru>
2025-04-09 20:42:31 +0300

    'feat/soldersCards'

--- src/consts/consts.ts
+++ src/consts/consts.ts
@@ -2,6 +2,6 @@ import Militiaman from 'images/russian-images/partizan.webp';
 
 export const russianMilitaryCards = [
   {
-    id: 1, img: { Militiaman }, title: 'Ополченец', damage: 2,
+    id: 1, img: { Militiaman }, title: 'Ополченец', damage: 2, armor: 0
   },
 ];
